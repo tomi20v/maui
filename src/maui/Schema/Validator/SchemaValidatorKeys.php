076856047526f422794d32fbb81d4dad943ed59a
t <tomi20v@gmail.com>
2014-09-14 13:38:26 +0200

    schemavalidatorkeys tested and fixed

--- src/maui/Schema/Validator/SchemaValidatorKeys.php
+++ src/maui/Schema/Validator/SchemaValidatorKeys.php
@@ -12,15 +12,19 @@ class SchemaValidatorKeys extends \SchemaValidatorTo {
 	}
 
 	public function apply($val, $Model=null) {
-		if (!$this->validate($val)) {
-			return null;
-		}
-		$val = array_intersect_key($val, array_flip($this->_value));
-		return $val;
+		return is_array($val) ? $val : array($val);
 	}
 
 	public function getError($val, $Model=null) {
 		return 'keys in array shall be {' . implode(', ', $this->_value) . ')';
+	}
+
+	public function filter($val, $Model=null) {
+		if (!is_array($val)) {
+			return null;
+		}
+		$val = array_intersect_key($val, array_flip($this->_value));
+		return $val;
 	}
 
 }
--- tests/maui/SchemaValidatorKeysTest.php
+++ tests/maui/SchemaValidatorKeysTest.php
@@ -14,17 +14,22 @@ class SchemaValidatorKeysTest extends \PHPUnit_Framework_TestCase {
 
 	/**
 	 * @dataProvider dataProvider
-	 * @covers SchemaValidatorKeys::apply
+	 * @covers SchemaValidatorKeys::filter
 	 */
-	function testApply($data, $filteredData, $isValid) {
+	function testFilter($data, $filteredData, $isValid) {
 		$keys = array('a', 'b');
 		$Validator = new \SchemaValidatorKeys($keys);
-		$this->assertEquals($filteredData, $Validator->apply($data));
+		$this->assertEquals($filteredData, $Validator->filter($data));
 	}
 
 	function dataProvider() {
 		return array(
 			array(array('a'=>1,'b'=>2), array('a'=>1,'b'=>2), true),
+			array(array('a'=>1), array('a'=>1), true),
+			array(array('a'=>1,'b'=>2, 'c'=>3), array('a'=>1,'b'=>2), false),
+			array(1, null, false),
+			array(array('x'=>9), array(), false),
+			array(array('a'=>1, 'x'=>9), array('a'=>1), false),
 		);
 	}
 
